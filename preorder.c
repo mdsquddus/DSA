0c9d94a825c6027580f5fc0e748e659252b39410
mdsquddus <mdshahzanquddus@gmail.com>
2026-08-23 03:55:42 +0530

    Add files via upload

---
+++ preorder.c
@@ -0,0 +1,44 @@
+// Online C compiler to run C program online 
+#include <stdio.h> 
+void push(int a); 
+int pop(); 
+int stack[100]; 
+int top =-1; 
+void push(int a) 
+{ 
+    top=top+1; 
+    stack[top]=a; 
+} 
+int pop(){ 
+    int a=stack[top]; 
+    top=top-1; 
+    return a; 
+} 
+int main() { 
+    printf("enter the number of elements in the binary tree\n"); 
+    int n; 
+    scanf("%d",&n); 
+    int a[n]; 
+    printf("enter the elements->\n"); 
+    for(int i=0;i<n;i++) 
+    { 
+        scanf("%d",&a[i]); 
+    } 
+    push(0); 
+    int i; 
+   while (top!=-1) 
+    { 
+        i=pop(); 
+        printf("%d->",a[i]); 
+        if((2*i+2)<n && a[2*i+2]!=0) 
+            push(2*i+2); 
+        if((2*i+1)<n && a[2*i+1]!=0) 
+            push(2*i+1); 
+         
+ 
+    } 
+
+    // Write C code here 
+ 
+    return 0; 
+} 
